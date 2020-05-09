1513dba31c1be71907dac256dad8e4ece0374b68
Charlie Conneely <Charlie Conneely>
2020-05-09 21:20:04 +0000

    verbose and quiet args added to cmd line

--- runner.py
+++ runner.py
@@ -5,13 +5,26 @@ import argparse
 import nfa
 
 parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+
+group.add_argument("-v", "--verbose", action="store_true")
+group.add_argument("-q", "--quiet", action="store_true")
 parser.add_argument("text", help="The string of text", type=str)
 parser.add_argument("regex", help="The regular expression", type=str)
 
 args = parser.parse_args()
 
-#text = input("Please enter your string of text: ")
-#regex = input("Please enter your regular expression: ")
 result = nfa.match(args.regex, args.text)
 
-print(result)
+if args.verbose:
+    if result == False:
+        print("The text " + args.text + \
+            " does not match the regular expression " + args.regex)
+    else:
+        print("The text " + args.text + \
+                " matches the regular expression " + args.regex)
+elif args.quiet:
+    print(str(result))
+else:
+    print("nfa.match(" + args.text + "," + args.regex + ") = " + str(result))
+
